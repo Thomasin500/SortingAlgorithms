862f168f5d59f315bd8be548cb435e653ce758d8
Thomas <tfreeman1@mail.usf.edu>
2015-11-09 00:01:55 -0500

    Implemented Selection Sort

--- Project 4 - Sorting/main.cpp
+++ Project 4 - Sorting/main.cpp
@@ -6,10 +6,12 @@
 //	Data Structures Project 4 - Sorting
 ///////////////////////////////////
 
+#include "sortingAlgorithms.h"
+#include <iostream>
+
 #include <vector>
 
 using namespace std;
-
 
 void permute(vector<int> & a) {
 
@@ -18,13 +20,22 @@ void permute(vector<int> & a) {
 
 bool checkSort(const vector<int> & a) {
 
-
+	return false;
 }
 
 
 int main() {
 
+	vector<int> array1 = {7, 3, 8, 1, 0, 5, 9, 2, 4, 6};
 
+	sortingAlgorithms sort;
+
+	sort.selectionSort(array1);
+
+	cout << "Done" << endl;
+
+	cin.ignore();
+	cin.ignore();
 
 	return 1;
 }
--- Project 4 - Sorting/sortingAlgorithms.h
+++ Project 4 - Sorting/sortingAlgorithms.h
@@ -1,26 +1,93 @@
 #pragma once
 
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class sortingAlgorithms {
 
+public:
+
+	sortingAlgorithms() {}
 
 	template <typename Comparable>
-	void selectionSort(vector<Comparable> & a);
+	bool checkSorted(vector<Comparable> & a) {
+
+
+
+		return false;
+	}
 
 	template <typename Comparable>
-	void bubbleSort(vector<Comparable> & a);
+	void display(vector<Comparable> & a) {
+
+		for (int i = 0; i < a.size(); i++) {
+
+			cout << a[i] << " ";
+		}
+
+		cout << "\n\n";
+	}
 
 	template <typename Comparable>
-	void insertionSort(vector<Comparable> & a);
+	void selectionSort(vector<Comparable> & a) {
+
+		cout << "Starting Selection Sort" << endl;
+	
+		for (int i = 0; i < a.size(); i++) {
+
+			int min = i;
+
+			for (int j = i; j < a.size(); j++) {
+
+				if (a[j] < a[min]) {
+
+					min = j;
+				}
+			}		   
+
+			swap(a[i], a[min]);
+		}
+
+		cout << "Selection Sort Finished:\n";
+		display(a);
+	}
 
 	template <typename Comparable>
-	void mergeSort(vector<Comparable> & a);
+	void bubbleSort(vector<Comparable> & a) {
+
+
+	}
 
 	template <typename Comparable>
-	void quickSort(vector<Comparable> & a);
+	void insertionSort(vector<Comparable> & a) {
+
+
+	}
 
 	template <typename Comparable>
-	void randomquickSort(vector<Comparable> & a);
+	void mergeSort(vector<Comparable> & a) {
+
+
+	}
 
 	template <typename Comparable>
-	void heapsort(vector<Comparable> & a);
+	void quickSort(vector<Comparable> & a) {
+
+
+	}
+
+	template <typename Comparable>
+	void sortingAlgorithms::randomquickSort(vector<Comparable> & a) {
+
+
+	}
+
+	template <typename Comparable>
+	void sortingAlgorithms::heapsort(vector<Comparable> & a) {
+
+
+	}
+
 };
